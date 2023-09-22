534757e0a885d4a3576ca60c974d5075222cab00
Fahad Farrukh <fahadfarrukh26@gmail.com>
2023-09-22 22:02:34 +0000

    commit

--- hello_cargo/src/example2.rs
+++ hello_cargo/src/example2.rs
@@ -85,5 +85,33 @@ while x < length {
 }
 
 println!("{}", greatest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fn reverse_string(input: &str) -> String {
+//     let reversed: String = input.chars().rev().collect();
+//     reversed
+// }
+
+// fn main() {
+//     let input_string = "Hello, World!";
+//     let reversed_string = reverse_string(input_string);
+//     println!("Original: {}", input_string);
+//     println!("Reversed: {}", reversed_string);
+// }
     
 }
