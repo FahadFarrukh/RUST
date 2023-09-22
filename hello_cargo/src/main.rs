f2cc1526f432f9273691601694b05fe08ee6ec3a
Fahad Farrukh <fahadfarrukh26@gmail.com>
2023-09-22 21:55:37 +0000

    commit

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,15 +1,13 @@
+#[derive(Debug)]
+struct Rectangle {
+width: u32,
+height: u32,
+}
 fn main() {
-    let numbers = [5, 8, 2, 10, 30];
-    let mut x = 0;
-    let length = numbers.len();
-    let mut greatest: i32 = numbers[0]; // Initialize with the first element
-
-    while x < length {
-        if numbers[x] > greatest {
-            greatest = numbers[x]; // Update if the current element is greater
-        }
-        x = x + 1;
-    }
-
-    println!("{}", greatest);
+let scale = 2;
+let rect1 = Rectangle {
+width: dbg!(30 * scale),
+height: 50,
+};
+dbg!(&rect1);
 }
