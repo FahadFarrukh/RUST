822a738b4db8d29334c529aab2d16a652cd032dd
Fahad Farrukh <fahadfarrukh26@gmail.com>
2023-09-23 08:38:39 +0000

    commit

--- hello_cargo/src/assessment.rs
+++ hello_cargo/src/assessment.rs
@@ -47,3 +47,25 @@ impl Date {
     
        
     }
+
+
+
+    // enum Coin{
+    //     Penny,
+    //     Nickel,
+    //     Dime,
+    //     Quarter
+    
+    // }
+    // fn value_in_cents(coin:Coin)->u8{
+    //     match coin{
+    //         Coin::Penny=>1,
+    //         Coin::Nickel=>5,
+    //         Coin::Dime=>10,
+    //         Coin::Quarter=>15,
+    //     }
+    //  }
+    // fn main(){
+    // let x =value_in_cents(Coin::Dime);
+    // println!("{}",x);
+    // }
--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,12 +1,27 @@
-enum Option<T> {
-    None,
-    Some(T)
+#[derive(Debug)]
+enum Usstate
+{
+    Alabama,
+    Alaska
 }
-
+enum Coin{
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(Usstate)
+}
+fn value_in_cents(coin:Coin)->u8{
+    match coin{
+        Coin::Penny=>1,
+        Coin::Nickel=>5,
+        Coin::Dime=>10,
+        Coin::Quarter(state)=>{
+            println!("State quarter from {:?}!",state);
+            25
+        },
+    }
+ }
 fn main(){
-    let x:i8=5;
-    let y:Option<i8>=Some(5);
-
-   let sum = x+y;
-
+let x =value_in_cents(Coin::Quarter(Usstate::Alaska));
+println!("{}",x);
 }
